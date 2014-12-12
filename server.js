e37d54f8ec6f8cdd6bfbd57a14c0bc938f510629
Jonathan Hensley <jhensley@gannett.com>
2014-12-12 08:22:58 -0500

    setup express

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-// a convenient variable to refer to the HTML directory
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
 var html_dir = './www/';
 
-// routes to serve the static HTML files
-app.get('/contact', function(req, res) {
-    res.sendfile(html_dir + 'contact.html');
+app.use(express.static(html_dir));
+
+app.get('/', function(req, res) {
+    res.sendFile(html_dir + 'index.html');
 });
-// Note: route names need not match the file name
-app.get('/hello', function(req, res) {
-    res.sendfile(html_dir + 'hello.html');
-});
+
+var server = app.listen(3000, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('Example app listening at http://%s:%s', host, port)
+
+})
