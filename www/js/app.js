cc1a47cde017e0b68b25478b13711674d1201041
Jonathan Hensley <jhensley@gannett.com>
2014-12-13 09:34:25 -0500

    getting some pages up and running

--- www/js/app.js
+++ www/js/app.js
@@ -3,8 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
-
+angular.module('shh', ['ionic'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -17,3 +16,16 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+.config(function($stateProvider, $urlRouterProvider) {
+    var pageDir = './pages/';
+
+    $stateProvider.state('home', {
+      url: '/',
+      templateUrl: pageDir + 'home.html'
+    });
+
+    $urlRouterProvider.otherwise("/");
+})
+.controller('PageController', ['$rootScope', function($rootScope) {
+  $rootScope.bodyClass='home-bg';
+}])
