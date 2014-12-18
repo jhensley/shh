3495034e712ba2e224834a9e996368c1679ced74
Jonathan Hensley <jhensley@gannett.com>
2014-12-18 08:18:35 -0500

    new images

--- www/js/app.js
+++ www/js/app.js
@@ -35,13 +35,13 @@ angular.module('shh', ['ionic', 'ionic.contrib.ui.cards', 'ngAnimate'])
     });
 
     $stateProvider.state('gifts', {
-      url: '/gifts/{id:[0-9]{1,4}}',
+      url: '/gifts/{id:[0-9]{1}}',
       templateUrl: pageDir + 'gifts.html',
       controller: 'GiftsController'
     });
 
     $stateProvider.state('hints', {
-      url: '/hints',
+      url: '/hints/{id:[0-9]{1}}',
       templateUrl: pageDir + 'gifts.html',
       controller: 'HintsController'
     });
@@ -176,6 +176,7 @@ angular.module('shh', ['ionic', 'ionic.contrib.ui.cards', 'ngAnimate'])
 }])
 .controller('GiftsController', ['$rootScope', '$scope', '$stateParams', '$state', function($rootScope, $scope, $stateParams, $state) {
   $rootScope.bodyClass='gifts-bg';
+  $rootScope.showHints = false;
   $scope.giftNumber = $stateParams.id;
   if ($stateParams.id == 1) {
     $scope.gift = "a Johnny Mercer CD";
@@ -196,100 +197,120 @@ angular.module('shh', ['ionic', 'ionic.contrib.ui.cards', 'ngAnimate'])
     $scope.nextGift = parseInt($stateParams.id)+1;
   }
 }])
-.controller('HintsController', ['$rootScope', function($rootScope) {
+.controller('HintsController', ['$rootScope', '$scope', '$stateParams', function($rootScope, $scope, $stateParams) {
   $rootScope.bodyClass='hints-bg';
   $rootScope.showHints = true;
   hasSeenHintsCards = true;
+
+  $scope.giftNumber = $stateParams.id;
+  if ($stateParams.id == 1) {
+    $scope.gift = "a Johnny Mercer CD";
+  }
+  if ($stateParams.id == 2) {
+    $scope.gift = "Forces of Nature";
+  }
+  if ($stateParams.id == 3) {
+    $scope.gift = "Chickens?";
+  }
+  if ($stateParams.id == 4) {
+    $scope.gift = "Midnight in the Garden of Good and Evil";
+  }
+  if ($stateParams.id == 5) {
+    $scope.gift = "a can of Honey Bee Salve";
+  }
+  if($stateParams.id < 5) {
+    $scope.nextGift = parseInt($stateParams.id)+1;
+  }
 }])
 .controller('CardsCtrl', ['$scope', '$rootScope', '$ionicSwipeCardDelegate', function($scope, $rootScope, $ionicSwipeCardDelegate) {
   var GiftOneCards = [{
     index: 0,
     title: 'This statue stands in <b>Ellis Square</b> - one of twenty-two squares of <b>his birthplace</b>.',
-    image: 'img/johnny-mercer-statue.jpg'
+    image: 'img/g1-1.jpg'
   }, {
     index: 1,
     title: 'Johnny was one of the founding members of Capitol Records in 1942.',
-    image: 'img/mercer-capitol.jpg'
+    image: 'img/g1-2.jpg'
   }, {
     index: 2,
     title: '<b>The Mercer House</b> built in 1860 still stands in <b>his hometown</b>, though he never lived there.',
-    image: 'img/mercer-house.jpg'
+    image: 'img/g1-3.jpg'
   }, {
     index: 3,
     title: 'Johhny and Ginger Mercer had a daughter ... named Mandy.',
-    image: 'img/mandy-mercer.jpg'
+    image: 'img/g1-4.jpg'
   }];
 
   var GiftTwoCards = [{
     index: 0,
     title: 'Ben Affleck and Sandra Bullock star as Ben Holmes and Sarah in this romantic comedy.',
-    image: 'img/bullock-affeck.jpg'
+    image: 'img/g2-1.jpg'
   }, {
     index: 1,
     title: "Bullock purchased this home after shooting the movie, loving <b>the film's final setting</b>.",
-    image: 'img/bullock-home.jpg'
+    image: 'img/g2-2.jpg'
   }, {
     index: 2,
-    title: 'On the trip, many setbacks prevent Ben and Sarah from getting to their destination.',
-    image: 'img/thunderstorm.jpg'
+    title: 'On the trip, many setbacks prevent Ben and Sarah from getting to <b>their destination</b>.',
+    image: 'img/g2-3.jpg'
   }, {
     index: 3,
     title: "The movie's tagline is: He went from the eye of the storm, into the arms of a hurricane.",
-    image: 'img/hurricane.jpg'
+    image: 'img/g2-4.jpg'
   }];
 
   var GiftThreeCards = [{
     index: 0,
     title: 'There are more chickens in the world than any other species of bird.',
-    image: 'img/rooster.jpg'
+    image: 'img/g3-1.jpg'
   }, {
     index: 1,
     title: 'The creator of these kitchen accessories has chickens at her home <b>off the Georgia coast</b>.',
-    image: 'img/paula-deen.jpg'
+    image: 'img/g3-2.jpg'
   }, {
     index: 2,
     title: 'Chickens are able to remember over 100 individuals; they can also recognise humans.',
-    image: 'img/smart-chicken.jpg'
+    image: 'img/g3-3.jpg'
   }, {
     index: 3,
     title: 'Chickens can’t taste sweetness in foods however they can detect salt, and most avoid it.',
-    image: 'img/salt.jpg'
+    image: 'img/g3-4.jpg'
   }];
 
   var GiftFourCards = [{
     index: 0,
     title: '<b>Locally</b> known as "The Book" in <b>the setting of this non-fiction based tale</b>.',
-    image: 'img/book.jpg'
+    image: 'img/g4-1.jpg'
   }, {
     index: 1,
     title: 'Once a drug store this cafe has become famous thanks to "The Book".',
-    image: 'img/clarys.jpg'
+    image: 'img/g4-2.jpg'
   }, {
     index: 2,
     title: 'Hotel tax revenues rose about twenty-five percent in the two years following the book.',
-    image: 'img/inn.jpg'
+    image: 'img/g4-3.jpg'
   }, {
     index: 3,
     title: "Like many other sites, <b>Bonaventure Cemetery</b> became famous after publication of the book.",
-    image: 'img/cemetery.jpg'
+    image: 'img/g4-4.jpg'
   }];
 
   var GiftFiveCards = [{
     index: 0,
     title: 'Honey contains all necessities sustain life: enzymes, vitamins, minerals, and water.',
-    image: 'img/honey.jpg'
+    image: 'img/g5-1.jpg'
   }, {
     index: 1,
     title: "You'll find several stores, including this brand's flagship store in <b>the company's namesake city</b>.",
-    image: 'img/bee-store.jpg'
+    image: 'img/g5-2.jpg'
   }, {
     index: 2,
     title: 'A bee flies to 1,000 flowers to make a single spoonful of honey.',
-    image: 'img/bee-flower.jpg'
+    image: 'img/g5-3.jpg'
   }, {
     index: 3,
     title: 'The <b>Savannah</b> Bee Company grew out of a passion for bees, honey and beekeeping.',
-    image: 'img/honey-love.jpg'
+    image: 'img/g5-5.jpg'
   }];
 
   if($scope.giftNumber == 1) {
@@ -310,51 +331,6 @@ angular.module('shh', ['ionic', 'ionic.contrib.ui.cards', 'ngAnimate'])
 
   if($scope.giftNumber == 5) {
     cardTypes = GiftFiveCards;
-  }
-
-  if($rootScope.showHints) {
-    cardTypes = [
-      {
-        index: 0,
-        title: 'CD: This statue stands in <b>Ellis Square</b> - one of twenty-two squares of <b>his birthplace</b>.',
-        image: 'img/johnny-mercer-statue.jpg'
-      },
-      {
-        index: 1,
-        title: 'CD: <b>The Mercer House</b> built in 1860 still stands in <b>his hometown</b>, though he never lived there.',
-        image: 'img/mercer-house.jpg'
-      },
-      {
-        index: 2,
-        title: "Movie: Bullock purchased this home after shooting the movie, loving <b>the film's final setting</b>.",
-        image: 'img/bullock-home.jpg'
-      },
-      {
-        index: 3,
-        title: 'Chickens: The creator of these kitchen accessories has chickens at her home <b>off the Georgia coast</b>.',
-        image: 'img/paula-deen.jpg'
-      },
-      {
-        index: 4,
-        title: 'Book: <b>Locally</b> known as "The Book" in <b>the setting of this non-fiction based tale</b>.',
-        image: 'img/book.jpg'
-      },
-      {
-        index: 5,
-        title: "Book: Like many other sites, <b>Bonaventure Cemetery</b> became famous after publication of the book.",
-        image: 'img/cemetery.jpg'
-      },
-      {
-        index: 6,
-        title: "Salve: You'll find several stores, including this brand's flagship store in <b>the company's namesake city</b>.",
-        image: 'img/bee-store.jpg'
-      },
-      {
-        index: 7,
-        title: 'Salve: The <b class="red">Savannah</b> Bee Company grew out of a passion for bees, honey and beekeeping.',
-        image: 'img/honey-love.jpg'
-      }
-    ]
   }
 
   $scope.cards = Array.prototype.slice.call(cardTypes, 0, 0);
